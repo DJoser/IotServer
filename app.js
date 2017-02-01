a6b9a375eadfc02882e566606ba0750762175930
Jose Ramon Navarro Marquez <ramonnavarropadilla@hotmail.com>
2017-01-31 20:18:35 -0600

    Se conecto socket.io a la aplicacion para chatear

--- app.js
+++ app.js
@@ -15,12 +15,32 @@ var express = require('express');
 // create a new express server
 var app = express();
 
+
+// Express server
+var http = require('http').Server(app);
+
+// Socket events User
+var userSocket = require('socket.io')(http);
+userSocket.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        socket.emit('chat message', msg);
+    });
+    socket.on('updateDevices',function (room) {
+        socket.emit('devices',['Foco1','Foco2']);
+    });
+});
+
+
 // User webpage
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
-
-var http = require('http').Server(app);
 http.listen(appEnv.port, '0.0.0.0', function(){
     console.log('server listening user on '+ appEnv.url);
 });
