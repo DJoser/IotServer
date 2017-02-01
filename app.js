5ab8edf2e57eaa748072b6f51652c1c0ca597fb7
Jose Ramon Navarro Marquez <ramonnavarropadilla@hotmail.com>
2017-01-31 20:10:06 -0600

    Se agrego la vista del chat a la aplicacion

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-/*eslint-env node*/
-
 //------------------------------------------------------------------------------
-// node.js starter application for Bluemix
+// node.js IotServer
 //------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+// create a new express server
+var app = express();
+
+// User webpage
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+var http = require('http').Server(app);
+http.listen(appEnv.port, '0.0.0.0', function(){
+    console.log('server listening user on '+ appEnv.url);
 });
